822a09b0288864b9bc956826a9b6a0a140bb5cf4
Wangquan <2962406390@qq.com>
2022-10-25 15:59:04 +0800

    这是一次新的提交测试

--- src/main/java/com/xitie/day11/Test02.java
+++ src/main/java/com/xitie/day11/Test02.java
@@ -2,13 +2,13 @@ package com.xitie.day11;
 
 /**
  * @program: xitie
- * @ClassName Test
+ * @ClassName Test02
  * @description: test
  * @author: 汪权
- * @create: 2022-10-25 08:54
+ * @create: 2022-10-25 15:50
  * @Version 1.0
  **/
-public class Test {
+public class Test02 {
     public static void main(String[] args) {
         System.out.println("hello world");
     }
