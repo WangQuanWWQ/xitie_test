608d7e001bd6d186fb77c137f4f3c33ef00747ec
Wangquan <2962406390@qq.com>
2022-10-25 15:45:26 +0800

    Initial commit

---
+++ src/main/java/com/xitie/day11/Test.java
@@ -0,0 +1,13 @@
+package com.xitie.day11;
+
+/**
+ * @program: xitie
+ * @ClassName Test
+ * @description: test
+ * @author: 汪权
+ * @create: 2022-10-25 08:54
+ * @Version 1.0
+ **/
+public class Test {
+
+}
